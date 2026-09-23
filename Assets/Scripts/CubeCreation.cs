4cd0e8244b8d0b05f35d6192100f65230648dd0e
Дмитрий <sde.rus45@gmail.com>
2026-09-23 22:22:17 +0500

    add unevenly cubes creation

--- Assets/Scripts/CubeCreation.cs
+++ Assets/Scripts/CubeCreation.cs
@@ -6,11 +6,13 @@ public class CubeCreation : MonoBehaviour
     [SerializeField] private GameObject prefab;
     [SerializeField] private int radius;
     [SerializeField] private bool isEvenly = true;
+    [SerializeField] private int unevenlyAngleStep;
 
     public int Radius => radius;
     public int CubeCount => cubeCount;
     public GameObject Prefab => prefab;
     public bool IsEvenly => isEvenly;
+    public int UnevenlyAngleStep => unevenlyAngleStep;
 
     private void Awake()
     {
@@ -29,20 +31,24 @@ public class CubeCreation : MonoBehaviour
 
     private void CreateCubes()
     {
+        float radianAngleStep;
         if (IsEvenly == true)
         {
-            var angleStep = 2 * Mathf.PI / CubeCount;
-            for (var i = 0; i < CubeCount; i++)
-            {
-                var x = transform.position.x + radius * Mathf.Cos(angleStep * i);
-                var z = transform.position.z + radius * Mathf.Sin(angleStep * i);
-                var position = new Vector3(x, transform.position.y, z);
-                var cube = Instantiate(Prefab, transform);
-                cube.transform.localPosition = position;
-                Debug.Log($"Куб {i} создан по координатам {position}");
-                cube.transform.SetParent(transform);
-            }
+            radianAngleStep = 2 * Mathf.PI / CubeCount;
         }
-
+        else
+        {
+            radianAngleStep = UnevenlyAngleStep * Mathf.PI / 180;
+        }
+        for (var i = 0; i < CubeCount; i++)
+        {
+            var x = transform.position.x + radius * Mathf.Cos(radianAngleStep * i);
+            var z = transform.position.z + radius * Mathf.Sin(radianAngleStep * i);
+            var position = new Vector3(x, transform.position.y, z);
+            var cube = Instantiate(Prefab, transform);
+            cube.transform.localPosition = position;
+            Debug.Log($"Куб {i} создан по координатам {position}");
+            cube.transform.SetParent(transform);
+        }
     }
 }
